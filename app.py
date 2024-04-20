60a0135e1d22dc5830fc99ee753f50e62f1d4538
Vivek Nair <vivekmanju53@gmail.com>
2024-04-21 00:56:59 +0530

    files init

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -10,5 +10,11 @@ STATUS = True
 def get_status():
     return jsonify({'status': STATUS})
 
+@app.route('/status', methods=['POST'])
+def update_status():
+    global STATUS
+    STATUS = request.json.get('status')
+    return jsonify({'status': STATUS})
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5050)
+    app.run(debug=True, host='0.0.0.0', port=5050)
