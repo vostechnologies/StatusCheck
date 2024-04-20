743eef7120cca74a9062c86d997626afc5b84226
Vivek Nair <vivekmanju53@gmail.com>
2024-04-21 01:35:23 +0530

    files init

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # Initialize the status
-global STATUS
-STATUS = True
-# Endpoint to get the status
+STATUS = "UNPAID"
+
 @app.route('/status', methods=['GET'])
 def get_status():
     return jsonify({'status': STATUS})
 
 @app.route('/status', methods=['POST'])
 def update_status():
+    global STATUS
     STATUS = request.json.get('status')
-    return jsonify({'status': STATUS})
+    return jsonify({'msg': "Status Changed Successfully!", 'status': STATUS})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5050)
